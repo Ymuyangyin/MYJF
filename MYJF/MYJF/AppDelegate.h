15d74bb9c5809273109c0a85870895626a930968
杨杨鹏 <yangyangpeng@yangpeng.local>
2017-12-22 15:17:12 +0800

    新建工程

---
+++ MYJF/MYJF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYJF
+//
+//  Created by 杨杨鹏 on 2017/12/22.
+//  Copyright © 2017年 杨杨鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYJF/MYJF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYJF
+//
+//  Created by 杨杨鹏 on 2017/12/22.
+//  Copyright © 2017年 杨杨鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
